cd335957874e44bcb670de8b9888f21dd74ffaeb
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2024-07-27 00:06:27 -0300

    Resolução atividade 10

--- exercicio10.php
+++ exercicio10.php
@@ -1,19 +1,24 @@
 <?php  
   
     function verificaPangrama($texto){
-        
+        $texto = strtolower($texto);
         $testes = array();
 
         foreach( range('a', 'z') as $elements) { 
             $resultado = strpos($texto, $elements);
-            echo $resultado;
+            if($resultado !== false) {   
+                array_push($testes, $resultado);
+            }
         } 
-        
-        
-        
-     
+    
+        if (count($testes) == 26){
+            echo "É pangrama";
+        } else{
+            echo "Não é." . " Faltam " . 
+            (26 - count($testes)) . " letras";
+        }
         
     }
-    verificaPangrama("abawdasd")
+    verificaPangrama("Tv faz quengo explodir com whisky jb");
     
 ?> 
